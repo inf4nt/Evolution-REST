a5dd6244d210ee51417f5860d6cac56d0c26d849
infantMG <com.infant@gmail.com>
2017-09-24 21:29:09 +0300

    fix db

--- src/main/java/evolution/config/AppConfig.java
+++ src/main/java/evolution/config/AppConfig.java
@@ -26,15 +26,15 @@ public class AppConfig {
 //        return DataSourceBuilder.create().build();
 //    }
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://ec2-176-34-113-15.eu-west-1.compute.amazonaws.com:5432/d1dsfee517idaf?sslmode=require&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
-        dataSource.setUsername("blnjznffbkytsy");
-        dataSource.setPassword("c119924e3525cce4b65884b042b6b83a6256f09e464b4767338d429d5adc6462");
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://ec2-176-34-113-15.eu-west-1.compute.amazonaws.com:5432/d1dsfee517idaf?sslmode=require&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+//        dataSource.setUsername("blnjznffbkytsy");
+//        dataSource.setPassword("c119924e3525cce4b65884b042b6b83a6256f09e464b4767338d429d5adc6462");
+//        return dataSource;
+//    }
 
     @Bean
     public ObjectMapper objectMapper() {
