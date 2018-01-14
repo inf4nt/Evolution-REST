fab1e28835cb063da4354962fe23d22ec426086e
infant <com.infant@gmail.com>
2018-01-14 23:49:38 +0200

    test 5

--- src/main/java/evolution/model/File.java
+++ src/main/java/evolution/model/File.java
@@ -1,25 +1,25 @@
-package evolution.model;
-
-import lombok.Data;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.Date;
-
-@Entity
-@Table(name = "file_data")
-@Data
-public class File {
-
-    @Id
-    @Column(name = "file_key", unique = true, updatable = false, columnDefinition = "varchar(255)")
-    private String fileKey;
-
-    @Column(name = "create_date", columnDefinition = "timestamp default current_timestamp")
-    private Date createDate;
-
-    @Column(name = "is_active", columnDefinition = "boolean default true")
-    private boolean isActive;
-}
+//package evolution.model;
+//
+//import lombok.Data;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//import java.util.Date;
+//
+//@Entity
+//@Table(name = "file_data")
+//@Data
+//public class File {
+//
+//    @Id
+//    @Column(name = "file_key", unique = true, updatable = false, columnDefinition = "varchar(255)")
+//    private String fileKey;
+//
+//    @Column(name = "create_date", columnDefinition = "timestamp default current_timestamp")
+//    private Date createDate;
+//
+//    @Column(name = "is_active", columnDefinition = "boolean default true")
+//    private boolean isActive;
+//}
