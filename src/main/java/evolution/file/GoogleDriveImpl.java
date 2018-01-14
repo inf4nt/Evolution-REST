9e29ccbad2e04e27a437066aee7291834d2d16ce
infant <com.infant@gmail.com>
2018-01-14 23:27:40 +0200

    stable 4

--- src/main/java/evolution/file/GoogleDriveImpl.java
+++ src/main/java/evolution/file/GoogleDriveImpl.java
@@ -29,7 +29,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class GoogleDriveImpl implements GoogleDrive {
